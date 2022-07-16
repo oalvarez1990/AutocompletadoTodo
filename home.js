9c7e2ac90e0296d2ef4a45725d85162ab27d7f20
Omar Aleiser Alvarez Laverde <omaraleyser@hotmail.com>
2022-07-15 22:20:09 -0500

    Prueba test

---
+++ home.js
@@ -0,0 +1,20 @@
+const tasks = [
+    {id:1, name:'Tarea 1',completed:true,assigned:'Carlos'},
+    {id:2, name:'Tarea 2',completed:false,assigned:'Maria'},
+    {id:3, name:'Tarea 3',completed:false,assigned:'Jose'},
+    {id:4, name:'Tarea 4',completed:false,assigned:'Crot'}, 
+    {id:5, name:'Tarea 5',completed:false,assigned:'Joe'},
+    {id:6, name:'Tarea 6',completed:false,assigned:'Carlos'},
+];
+function TodoList() {
+    const deleteTask = () =>{
+        //todo deleteTask
+    }
+    const updateTask = () =>{
+        //todo updateTask
+    }
+    for (let tasks of tasks){
+        //todo loop through tasks
+    }
+
+}
